0689975263402291ea5184bef3fcb81c4705f739
Rauf-pro <87373669+Rauf-pro@users.noreply.github.com>
2022-01-05 00:47:09 +0400

    Added Identity

--- BackEndProje/BackEndProje/Areas/Admin/Controllers/AccountController.cs
+++ BackEndProje/BackEndProje/Areas/Admin/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using BackEndProje.DATA;
+using BackEndProje.Models;
+using BackEndProje.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,22 +12,108 @@ using System.Threading.Tasks;
 namespace BackEndProje.Areas.Admin.Controllers
 {
     [Area("Admin")]
+  
     public class AccountController : Controller
     {
-        private UserManager<AppDbContext> _userManager;
+        
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(UserManager<AppDbContext> userManager)
+        public AccountController( UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
+            
             _userManager = userManager;
+            _signInManager = signInManager;
+          
+           
         }
-        public IActionResult Index()
+        public IActionResult Register()
         {
-            return View(_userManager.Users);
-      
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(VmRegister model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(model);
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(VmLogin user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, user.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(user);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login");
         }
 
 
-       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
--- BackEndProje/BackEndProje/ViewModels/VmLogin.cs
+++ BackEndProje/BackEndProje/ViewModels/VmLogin.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BackEndProje.ViewModels
 {
-    public class VmRegister
+    public class VmLogin
     {
         [Required]
         [EmailAddress]
@@ -15,9 +15,7 @@ namespace BackEndProje.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm Password")]
-        [Compare("Password", ErrorMessage = "Password and confirmation password not match.")]
-        public string ConfirmPassword { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
     }
 }
